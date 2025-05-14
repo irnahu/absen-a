a1b5240f620efd32f5b76fa85a57ddf593ecb6c6
farhan <farhanhafidzan83@gmail.com>
2025-05-14 16:13:27 +0700

    yahahaha ga boleh pulang

--- keuangan/urls.py
+++ keuangan/urls.py
@@ -14,7 +14,8 @@ urlpatterns = [
     path('rekap/', views.rekap_keuangan, name='rekap_keuangan'),
     path('export-pembayaran-excel/', views.export_pembayaran_excel, name='export_pembayaran_excel'),
     path('dashboard-admin/pembayaran/', views.kelola_pembayaran, name='kelola_pembayaran'),
-    path('siswa/tagihan/', views.tagihan_spp, name='tagihan_spp')
+    path('siswa/tagihan/', views.tagihan_spp, name='tagihan_spp'),
+    path('tagihan/', views.tagihan_orangtua, name='tagihan'),
 ]
 
 if settings.DEBUG:
